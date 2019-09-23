b332840d18a3e9806e98f07bf9d91886536e3630
Tera Schaller <tschaller722@gmail.com>
2019-09-23 16:53:23 -0500

    Set up React and added a landing page

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Dev Demo</h1>
+          <p className='lead'>
+            Demo of web development skills with React.js, Node.js, and MongoDB.
+            Create a developer profile, write posts and comments, and like your
+            favorite posts.
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
